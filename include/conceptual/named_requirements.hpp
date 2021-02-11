edb06c7758c95f6610587aa28f6959ff6a8f93c9
Hamondorf <wmh.stevens@gmail.com>
2021-02-11 10:17:36 -0500

    Added nreq_allocator (concept for the allocator named requirement)

--- include/conceptual/named_requirements.hpp
+++ include/conceptual/named_requirements.hpp
@@ -1,5 +1,4 @@
 #pragma once
 
-#include "conceptual/named_requirements/allocator.hpp"
+//#include "conceptual/named_requirements/allocator.hpp"
 #include "conceptual/named_requirements/container.hpp"
-#include "conceptual/named_requirements/nullable_pointer.hpp"
